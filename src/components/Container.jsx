8fe332f53f5001f3c160ad4a6b08c079b5d362d5
MariaVeselova-wbdv <mariaveselova.usm@gmail.com>
2025-10-30 18:39:59 +0300

    First commit

---
+++ src/components/Container.jsx
@@ -0,0 +1,9 @@
+const Container = () => {
+  return (
+    <header className="max-w-lg mx-auto mt-10 p-6 bg-my-primary rounded-lg shadow-lg text-center">
+     <h1>React Typicode Todos</h1>
+    </header>
+  );
+};
+
+export default Container;
