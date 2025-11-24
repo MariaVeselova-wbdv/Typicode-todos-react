724d32506df960c316fd31dd40b0d4c4a87170f5
MariaVeselova-wbdv <mariaveselova.usm@gmail.com>
2025-11-24 18:23:19 +0300

    Todo-delete

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,60 @@ import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
 const App = () => {
-  const apiUrl = 'https://jsonplaceholder.typicode.com/todos'; 
+  const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
   //* Todos
   const [todos, setTodos] = useState([]);
 
+  const deleteTodo = async (id) => {
+    const confirmDelete = window.confirm('Are you sure to delete?');
+    if (confirmDelete) {
+      try {
+        await fetch(apiUrl + '?_limit=5', {
+          method: 'DELETE'
+        });
+
+      setTodos(todos.filter((todo) => todo.id !== id));
+      } catch (error) {
+        console.error('Error deleting todo:', error);
+      }
+    }
+  };
+
   // * API
+  // useEffect(() => {
+  //   const getTodos = () => {
+  //     fetch(apiUrl + '?_limit=5')
+  //       .then((res) => res.json())
+  //       .then((data) => setTodos(data))
+
+  //       .catch((error) => console.error('Error fetching todos:', error));
+  //   };
+
+  //   getTodos();
+  // }, []);
+
   useEffect(() => {
-    const getTodos = () => {
-      
-      fetch(apiUrl + '?_limit=5')
-      .then(res => res.json())
-      .then(data => setTodos(data))
-   
-      .catch(error => console.error('Error fetching todos:', error));
-     
+    const getTodos = async() => {
+      try{
+        const res = await fetch(apiUrl + '?_limit=5')
+        const data = await res.json();
+        setTodos(data)
+      } catch(error){
+          console.error('Error fetching todos', error)
+      }
     };
 
-    getTodos();
-  }, [])
+    getTodos()
+  }, []);
+
+
 
   return (
     <div className='max-w-lg mx-auto mt-10 p-6 rounded-lg shadow-lg bg-my-primary'>
       <Header />
       <TodoForm todos={todos} setTodos={setTodos} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
     </div>
   );
 };
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,4 @@
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, deleteTodo }) => {
   if(todos.length === 0){
     return (
       <p className="text-center text-gray-700">No Todos Yet</p>
@@ -7,7 +7,7 @@ const TodoList = ({ todos }) => {
   return ( 
     <div className="mt-5 cursor-pointer">
       { todos.map((todo) => (
-        <div key={todo.id} className="bg-my-secondary w-full flex  cursor-pointer text-gray-800 rounded shadow-lg border-l-4 mb-2 p-1">
+        <div key={todo.id} onDoubleClick={() => deleteTodo(todo.id)}  className="bg-my-secondary w-full flex  cursor-pointer text-gray-800 rounded shadow-lg border-l-4 mb-2 p-1">
           <h3 className="text-sm font-medium">{ todo.todo || todo.title}</h3>
         </div>
       ) )
